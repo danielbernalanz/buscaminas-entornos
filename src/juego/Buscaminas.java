c5875d2171bb0e493011e003b2cf9be97d72e5c7
walid sbaa zourar <walidsbaa0517@gmail.com>
2025-12-18 13:54:21 +0100

    Menu hecho

--- src/juego/Buscaminas.java
+++ src/juego/Buscaminas.java
@@ -6,6 +6,27 @@ public class Buscaminas {
 	 * 
 	 */
 	public static void main(String[] args) {
+		
+		String menu = "menu" ; 
+	  		
+	    switch (menu) {
+		case "Poner bandera":
+			System.out.println ("el usuario ha puesto una bandera"); 
+			break; 
+			
+		case "Descubrir" :
+			System.out.println ("Se ha descubierto una casilla") ; 
+			break;
+		case "Quitar bandera":
+			System.out.println ("el usuario a quitado una bandera") ; 
+			break; 
+			
+			
+			
+		
+			
+		}
+		
 
 	}
 }
