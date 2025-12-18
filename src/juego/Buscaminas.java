e195600eedda9cbb083996e829f9ad16809831a3
Raul <slimerxo@gmail.com>
2025-12-18 14:20:18 +0100

    Actualizado menu

--- src/juego/Buscaminas.java
+++ src/juego/Buscaminas.java
@@ -1,11 +1,44 @@
 package juego;
 
-public class Buscaminas {
-	/*
-	 * Gestiona la interacción con el usuario, el menú de opciones…
-	 * 
-	 */
-	public static void main(String[] args) {
 
-	}
+import java.util.Scanner;
+
+public class Buscaminas {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        boolean funcionamiento = true;
+        System.out.println("Con cuantas filas desea jugar?");
+        int filasInicio = sc.nextInt();
+        System.out.println("Con cuantas columnas desea jugar?");
+        int columnasInicio = sc.nextInt();
+        System.out.println("Con cuantas minas desea jugar?");
+        int minas = sc.nextInt();
+        Juego buscaminas = new Juego(minas, filasInicio, columnasInicio);
+        while (funcionamiento) {
+            System.out.println("0.Salir");
+            System.out.println("1.Descubrir");
+            System.out.println("2.Poner Bandera");
+            System.out.println("3.Quitar Bandera");
+            int opcion = sc.nextInt();
+            switch (opcion) {
+                case 0:
+                    funcionamiento = false;
+                    break;
+                case 1:
+                    System.out.println("Fils de casilla a descubrir (empieza en 0)");
+                    int fila = sc.nextInt();
+                    System.out.println("Columna de casilla a descubrir (empieza en 0)");
+                    int columna = sc.nextInt();
+                    buscaminas.descubrirCasilla(fila, columna);
+                    break;
+                case 2:
+
+                    break;
+                case 3:
+
+                    break;
+            }
+        }
+    }
 }
--- src/juego/Juego.java
+++ src/juego/Juego.java
@@ -2,59 +2,82 @@ package juego;
 
 public class Juego {
 
-    Tablero tableroReal;
-    Tablero tableroUsuario;
-    boolean juego;
-    private int numBanderas; // el usuario debera poner tantas banderas como minas y en las posiciones de las minas
-    private int casillasPorVisitar; //
+	private Tablero tableroReal;
+	private Tablero tableroUsuario;
+	private boolean juego;
+	private int numBanderas; // el usuario debera poner tantas banderas como minas y en las posiciones de las
+								// minas
+	private int casillasPorVisitar; // las casillas que al jugador le quedan por revelar
 
-    /*
+	/*
 	 * El constructor se encargará de crear el tablero inicial del Juego para poder
 	 * empezar a jugar.
-     */
-    public Juego(int numMinas, int numFilas, int numColumnas) {
-        tableroReal = new Tablero(numMinas, numFilas, numColumnas);
-        tableroUsuario = new Tablero(numMinas, numFilas, numColumnas);
-        tableroUsuario.crearTableroVacio();
-        tableroReal.inicializarTablero();
-        numBanderas = numMinas;
-        juego = true; // cuando sea false el juego para
-        casillasPorVisitar = (tableroReal.getTablero().length * tableroReal.getTablero()[0].length) - numMinas;
-    }
+	 */
+	public Juego(int numMinas, int numFilas, int numColumnas) {
+		tableroReal = new Tablero(numMinas, numFilas, numColumnas);
+		tableroUsuario = new Tablero(numMinas, numFilas, numColumnas);
+		tableroUsuario.crearTableroVacio();
+		tableroReal.inicializarTablero();
+		numBanderas = numMinas;
+		juego = true; // cuando sea false el juego para
+		casillasPorVisitar = (tableroReal.getTablero().length * tableroReal.getTablero()[0].length) - numMinas;
+	}
 
-    public boolean descubrirCasilla(int fila, int columna) {
-        if (!tableroUsuario.getTablero()[fila][columna].isVisible()) {
-            if (!tableroUsuario.getTablero()[fila][columna].isBandera()) {
-                if (tableroUsuario.getTablero()[fila][columna].isMina()) {
-                    juego = false;
-                    tableroReal.toString();//Muestra el tablero con las bombas (el tablero del sistema)
-                    return false;//Ha perdido ya que exploto una mina
-                }
-                tableroUsuario.getTablero()[fila][columna].setVisible(true); //se le muestra la casilla al usuario
-                casillasPorVisitar--;
-                
-                
-                if (casillasPorVisitar == 0 && numBanderas == 0) { // si ya ha descubierto todas las casillas y ha puesto todas las banderas ne las bombas
-                    juego = false; // el juego se acaba
-                }
-            }// hay bandera, por lo que no se puede revelar
-        }
-        return false; //la casilla ya es visible o datos erroneos
-    }
+	public boolean descubrirCasilla(int fila, int columna) {
+		if (fila > tableroReal.getTablero().length || columna > tableroReal.getTablero()[fila].length || fila < 0
+				|| columna < 0) {
+			if (!tableroUsuario.getTablero()[fila][columna].isVisible()) {
+				if (!tableroUsuario.getTablero()[fila][columna].isBandera()) {
+					if (tableroUsuario.getTablero()[fila][columna].isMina()) {
+						juego = false;
+						tableroReal.toString();// Muestra el tablero con las bombas (el tablero del sistema)
+						return false;// Ha perdido ya que exploto una mina
+					}
+					tableroUsuario.getTablero()[fila][columna].setVisible(true); // se le muestra la casilla al usuario
+																					// mostrando numero
+					casillasPorVisitar--;
+					if (tableroUsuario.getTablero()[fila][columna].isBlanco()) { // si es vacia se muestran las que lo
+																					// rodean
+						descubrirCasilla(fila - 1, columna - 1);
+						descubrirCasilla(fila - 1, columna);
+						descubrirCasilla(fila - 1, columna + 1);
+						descubrirCasilla(fila, columna - 1);
+						descubrirCasilla(fila, columna + 1);
+						descubrirCasilla(fila + 1, columna - 1);
+						descubrirCasilla(fila + 1, columna + 1);
+					}
+					if (casillasPorVisitar == 0 && numBanderas == 0) { // si ya ha descubierto todas las casillas y ha
+																		// puesto todas las banderas ne las bombas
+						juego = false; // el juego se acaba
+					}
+				} // hay bandera, por lo que no se puede revelar
+			}
+			return false; // la casilla ya es visible o datos erroneos
+		}
+		return false; // la casilla esta fuera del tablero
+	}
 
-    public void ponerBandera(int fila, int columna) {
-        tableroUsuario.getTablero()[fila][columna].setBandera(true);
-        if (tableroUsuario.getTablero()[fila][columna].isMina()) {
-            numBanderas--;
-            if (numBanderas == 0 && casillasPorVisitar == 0) { // si ya puso tantas banderas como bombas y no quedan casillas por visitar gano
-                juego = false;
-            }
-        }
-    }
+	public void ponerBandera(int fila, int columna) {
+		if (!tableroUsuario.getTablero()[fila][columna].isVisible()) {// solo bandera si la casilla no es visible
+			tableroUsuario.getTablero()[fila][columna].setBandera(true);
+			if (tableroUsuario.getTablero()[fila][columna].isMina()) {
+				numBanderas--;
+				if (numBanderas == 0 && casillasPorVisitar == 0) { // si ya puso tantas banderas como bombas y no quedan
+																	// casillas por visitar gano
+					juego = false;
+				}
+			}
+		}
+	}
 
-    public void quitarBandera(int fila, int columna) {
-        tableroUsuario.getTablero()[fila][columna].setBandera(false);
-        numBanderas++; // no hace falta comrpobar la victoria ya que se gana poniendo una bandera o revelando una casilla, en ningún otro caso se gana
-    }
+	public boolean getJuego() {
+		return juego;
+	}
+
+	public void quitarBandera(int fila, int columna) {
+		tableroUsuario.getTablero()[fila][columna].setBandera(false);
+		numBanderas++; // no hace falta comrpobar la victoria ya que se gana poniendo una bandera o
+						// revelando una casilla, en ningún otro caso se gana
+	}
 
 }
