a7b11374b514ca379b4cad833c64d316a399e2f8
Raul <slimerxo@gmail.com>
2025-12-18 13:44:13 +0100

    Cambios hechos por raul

--- src/juego/Juego.java
+++ src/juego/Juego.java
@@ -1,37 +1,60 @@
 package juego;
 
 public class Juego {
-	Tablero tablero;
 
-	/*
+    Tablero tableroReal;
+    Tablero tableroUsuario;
+    boolean juego;
+    private int numBanderas; // el usuario debera poner tantas banderas como minas y en las posiciones de las minas
+    private int casillasPorVisitar; //
+
+    /*
 	 * El constructor se encargará de crear el tablero inicial del Juego para poder
 	 * empezar a jugar.
-	 */
-	public Juego(Tablero tablero) {
-		this.tablero = tablero;
-	}
+     */
+    public Juego(int numMinas, int numFilas, int numColumnas) {
+        tableroReal = new Tablero(numMinas, numFilas, numColumnas);
+        tableroUsuario = new Tablero(numMinas, numFilas, numColumnas);
+        tableroUsuario.crearTableroVacio();
+        tableroReal.inicializarTablero();
+        numBanderas = numMinas;
+        juego = true; // cuando sea false el juego para
+        casillasPorVisitar = (tableroReal.getTablero().length * tableroReal.getTablero()[0].length) - numMinas;
+    }
 
-	public boolean descubrirCasilla() {
-		boolean tieneMina = false;
-		boolean tieneNumero = false;
-		if (tieneMina) {
-			// Mostrar minas
-			// Terminar juego
-		} else if (tieneNumero) {
-			// Mostrar número de minas adyacentes
-		} else {
-			// Descubrir automáticamente el área de casillas blancas adyacentes
-		}
+    public boolean descubrirCasilla(int fila, int columna) {
+        if (!tableroUsuario.getTablero()[fila][columna].isVisible()) {
+            if (!tableroUsuario.getTablero()[fila][columna].isBandera()) {
+                if (tableroUsuario.getTablero()[fila][columna].isMina()) {
+                    juego = false;
+                    tableroReal.toString();//Muestra el tablero con las bombas (el tablero del sistema)
+                    return false;//Ha perdido ya que exploto una mina
+                }
+                tableroUsuario.getTablero()[fila][columna].setVisible(true); //se le muestra la casilla al usuario
+                casillasPorVisitar--;
+                
+                
+                if (casillasPorVisitar == 0 && numBanderas == 0) { // si ya ha descubierto todas las casillas y ha puesto todas las banderas ne las bombas
+                    juego = false; // el juego se acaba
+                }
+            }// hay bandera, por lo que no se puede revelar
+        }
+        return false; //la casilla ya es visible o datos erroneos
+    }
 
-		return true;
-	}
+    public void ponerBandera(int fila, int columna) {
+        tableroUsuario.getTablero()[fila][columna].setBandera(true);
+        if (tableroUsuario.getTablero()[fila][columna].isMina()) {
+            numBanderas--;
+            if (numBanderas == 0 && casillasPorVisitar == 0) { // si ya puso tantas banderas como bombas y no quedan casillas por visitar gano
+                juego = false;
+            }
+        }
+    }
 
-	public void ponerBandera() {
-		
-	}
-
-	public void quitarBandera() {
-
-	}
+    public void quitarBandera(int fila, int columna) {
+        tableroUsuario.getTablero()[fila][columna].setBandera(false);
+        numBanderas++; // no hace falta comrpobar la victoria ya que se gana poniendo una bandera o revelando una casilla, en ningún otro caso se gana
+    }
 
 }
--- src/juego/Tablero.java
+++ src/juego/Tablero.java
@@ -1,44 +1,67 @@
 package juego;
 
 import java.util.Arrays;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class Tablero {
-	private Casilla[][] tablero;
-	private int numMinas;
-	private int numFilas;
-	private int numColumnas;
 
-	public Tablero(int numMinas, int numFilas, int numColumnas) {
-		/*
-		 * El constructor se encargará de inicializar cada atributo según parámetros y
-		 * creará el tablero con Casillas “vacías”,
-		 */
-		this.numMinas = numMinas;
-		this.numFilas = numFilas;
-		this.numColumnas = numColumnas;
-		crearTableroVacio();
-	}
+    private Casilla[][] tablero;
+    private int numMinas;
+    private int numFilas;
+    private int numColumnas;
 
-	/*
-	 * Devolve el tablero como cadena
-	 */
-	public String toString() {
-		return "Tablero [tablero=" + Arrays.toString(tablero) + ", numMinas=" + numMinas + ", numFilas=" + numFilas
-				+ ", numColumnas=" + numColumnas + "]";
-	}
+    /*
+	 * El constructor se encargará de inicializar cada atributo según parámetros y
+	 * creará el tablero con Casillas “vacías”,
+     */
+    public Tablero(int numMinas, int numFilas, int numColumnas) {
+        this.numMinas = numMinas;
+        this.numFilas = numFilas;
+        this.numColumnas = numColumnas;
+    }
 
-	/*
+    /*
+    * Devuelve el tablero como cadena
+     */
+    public String toString() {
+        return "Tablero [tablero=" + Arrays.toString(tablero) + ", numMinas=" + numMinas + ", numFilas=" + numFilas
+                + ", numColumnas=" + numColumnas + "]";
+    }
+
+    /*
 	 * Este metodo añadirá minas de forma aleatoria y calculará los números de cada
 	 * casilla en función de las minas que haya en las casillas de alrededores.
-	 */
+     */
+    public void crearTableroVacio() {//inicialización del tablero del usuario
+        tablero = new Casilla[numFilas][numColumnas];
+        for (int i = 0; i < tablero.length; i++) {
+            for (int j = 0; j < tablero[0].length; j++) {
+                tablero[i][j].setVisible(false);
+            }
+        }
+    }
 
-	public void crearTableroVacio() {
-		tablero = new Casilla[numFilas][numColumnas];
+    public void inicializarTablero() {//inicializacion del tablero del sistema
+        //Colocación de las minas aleatorias
+        int casillasTot = tablero.length * tablero[0].length;
+        for (int i = 0; i < numMinas; i++) {
+            int mina = ThreadLocalRandom.current().nextInt(0, casillasTot);
+            int filaMina = mina / tablero[0].length;
+            int anchuraMina = mina % tablero[0].length;
+            if (!tablero[filaMina][anchuraMina].isMina()) { // evita repetidos
+                tablero[filaMina][anchuraMina].setMina(true);
+            }
+        }
+        
+        for (int i = 0; i < tablero.length; i++) {
+            for (int j = 0; j < tablero[0].length; j++) {
+                tablero[i][j].setVisible(true); // todas las casillas visibles, para cuando se llame al toSting
+            }
+        }
+    }
 
-	}
-
-	public void inicializarTablero() {
-
-	}
+    public Casilla[][] getTablero() {
+        return tablero;
+    }
 
 }
